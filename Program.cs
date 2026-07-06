cb0b116db7090acde59a48f55a5aafa5c49cf1c6
ndq3004 <ndq3004@gmail.com>
2026-07-07 00:32:48 +0700

    add test code

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+var except = 0;
+
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
